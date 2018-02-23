b0acf4f8e0696df2808083c0b0c2c864dffcdecb
jeremyleondsst <31115848+jeremyleondsst@users.noreply.github.com>
2018-02-22 22:12:56 -0700

    Add files via upload

---
+++ Final.js
@@ -0,0 +1,11 @@
+function initMap() {
+  if (map) {
+   var map = new google.maps.Map(document.getElementById('map'), {
+     center: {
+       lat: -13.1631412,
+       lng: -72.5471516
+     },
+     zoom: 5
+   });
+}
+ }
